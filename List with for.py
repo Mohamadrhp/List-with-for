597e373d561e475a19790c8a6373593f11c3538f
Mohamadrhp <132127164+Mohamadrhp@users.noreply.github.com>
2024-10-27 09:31:49 +0330

    Add files via upload

---
+++ List with for.py
@@ -0,0 +1,24 @@
+# #Example1:
+Names = ["Mohamad","Amir","Reza","Hossein","Javad"]
+for esm in Names:
+    print(esm)
+
+# #Example2:
+Seasons = ["Spring","Summer","Fall","Winter"]
+for fasl in Seasons:
+    print(fasl)
+
+#Example3:
+Books = ["Ryazi","Farsi","Fizik","Englisi"]
+for ketab in Books:
+    print(ketab)
+
+# #Example4:
+Cars = ["BMW","Benz","Toyota","Pride"]
+for mashin in Cars:
+    print(mashin)
+
+# #Example5:
+City = ["Qaemshahr","Babol","Amool","Neka"]
+for shahr in City:
+    print(shahr)            
